d7aec772910320459050066519b1a1793771f790
Franco Damilano <francodemilano@gmail.com>
2022-09-20 06:18:44 -0300

    https://github.com/francodemilano/Franqoise.git

---
+++ src/Components/Avatar.js
@@ -0,0 +1,12 @@
+const AvatarJsx = (params) => {
+    const src = `https://randomuser.me/api/portraits/lego/1.jpg`
+
+    return (
+        <picture>
+            <img src={src} alt='Fran'/>
+            Seba
+        </picture>
+    )
+}
+
+export default AvatarJsx
