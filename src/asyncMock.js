9978c812680d62fabda953d7dd28ede27a18371a
Franco Damilano <francodemilano@gmail.com>
2022-10-26 07:58:25 -0300

    agregado de productos

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -25,6 +25,33 @@ const products = [
         img: 'https://http2.mlstatic.com/D_NQ_NP_992933-MLA51065256745_082022-W.jpg',
         stock: 5,
         description: 'Zapatilla usada'
+    },
+    {
+        id: '4',
+        name: 'Sony XPeria A1',
+        price: 58000,
+        category: 'Celulares',
+        img: 'https://el-salvador.tiendacelular.com/oc-content/uploads/903/1626476.jpg',
+        stock: 2,
+        description: '2 años de uso, estado como nuevo'
+    },
+    {
+        id: '5',
+        name: 'Iphone 11 pro max',
+        price: 218000,
+        category: 'Celulares',
+        img: 'https://http2.mlstatic.com/D_NQ_NP_976221-MLA51413252946_092022-V.jpg',
+        stock: 1,
+        description: '1 años de uso, estado como nuevo, bateria el 90%'
+    },
+    {
+        id: '6',
+        name: 'Camiseta Seleccion Mundial 90',
+        price: 12000,
+        category: 'Indumentaria',
+        img: 'https://www.diariosintesis.com.ar/images/2017/11/06/camisetaargentina1990.jpg',
+        stock: 2,
+        description: 'Copia +AAA, NUEVA!'
     }
 ]
 
