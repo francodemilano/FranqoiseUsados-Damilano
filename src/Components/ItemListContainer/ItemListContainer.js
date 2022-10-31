6574e94ddf18e68bc4daf9b65a4521d54aa1a9d2
Franco Damilano <francodemilano@gmail.com>
2022-10-31 09:07:26 -0300

    add products

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,7 @@
-import './ItemListContainer.css'
-import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect, useContext } from 'react' 
 import ItemList from '../ItemList/ItemList'
-import { useParams } from 'react-router-dom' 
+import { useParams } from 'react-router-dom'
+import './ItemListContainer.css'
 import { getDocs, collection, query, where } from 'firebase/firestore'
 import { db } from '../../service/firebase'
 import { NotificationContext } from '../../Notification/Notification'
@@ -9,47 +9,49 @@ import { NotificationContext } from '../../Notification/Notification'
 const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
-    
-    const { setNotification } = useContext(NotificationContext)
+
+const { setNotification } = useContext(NotificationContext)
 
     const { categoryId } = useParams()
-
-    useEffect(() => {
+    
+    useEffect(() =>{
         setLoading(true)
 
-        const collectionRef = categoryId 
-                ? query(collection (db, 'products'), where('categories', '==', categoryId))
-                : collection (db, 'products',)
-  
+        const collectionRef = categoryId  
+        ? query(collection(db, 'products'), where('category', '==', categoryId) )
+        : collection(db, 'products')
 
         getDocs(collectionRef).then(response => {
-          
+            console.log(response)
+
             const productsAdapted = response.docs.map(doc => {
-                const data = doc.data ()
-                return {id: doc.id, ...data}
+                const data = doc.data()
+                console.log(data)
+                return { id: doc.id, ...data}
             })
+
             setProducts(productsAdapted)
         })
         .catch(error => {
-            setNotification('error', 'No se pueden obtener los productos')
+            setNotification('error', 'No pudimos acceder a los productos')
         })
-        .finally(() =>{
+        .finally(() => {
             setLoading(false)
         })
- 
+
     }, [categoryId])
 
 
-
-    if(loading && true) {
-        return <h1>Cargando productos...</h1>
+    if(loading && true) {        
+        return <h1>Loading...</h1>
     }
 
 
+
     return (
-        <div>
-            <h1>{`${greeting} ${categoryId || ''}`}</h1>
-            <ItemList products={products} />
+        <div className="ItemListContainer" onClick={() => console.log('hice click en el ItemListContainer')}>
+            <h2>{`${greeting} ${categoryId || ''}`}</h2>
+                { products.length > 0 && <ItemList products={products} />}
         </div>
     )
 }
